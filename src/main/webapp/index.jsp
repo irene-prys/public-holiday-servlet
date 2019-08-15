be8006e41973e00349f6e9254e465ce1172d2bb8
iryna prys <iryna.prys@sigma.software>
2019-08-15 14:44:23 +0300

    Add service to communicate with Alexa

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello, from Public Holidays!</h2>
+</body>
+</html>
---
+++ src/main/webapp/java/demo/holidays/PublicHolidaysServlet.java
@@ -0,0 +1,21 @@
+package demo.holidays;
+
+import com.amazon.ask.Skill;
+import com.amazon.ask.Skills;
+
+import com.amazon.ask.servlet.SkillServlet;
+import demo.holidays.handler.HolidaysHandler;
+
+public class PublicHolidaysServlet extends SkillServlet {
+
+    public PublicHolidaysServlet() {
+        super(getSkill());
+    }
+
+    private static Skill getSkill() {
+        return Skills.standard()
+                .addRequestHandlers(
+                        new HolidaysHandler())
+                .build();
+    }
+}
